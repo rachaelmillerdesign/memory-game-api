a7ba018e1c4ebc6d0f4be926edb934041df53604
rachael miller design <rachael@rachaelmillerdesign.com>
2018-08-20 19:37:38 -0400

    creating files to enable csv processing

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,2 +1,17 @@
+require 'csv'
+
 class Creature < ApplicationRecord
+  def initialize(h)
+    h.each_key do |key|
+      instance_variable_set(('@' + key.to_s).to_sym, h[key])
+      self.class.send(:attr_reader, key)
+    end
+  end
+end
+creature = []
+CSV.foreach('data/creature.csv',
+            headers: true,
+            header_converters: ->(h) { h.downcase.to_sym }) do |person|
+              people << Person.new(person.to_hash)
+            end
 end
