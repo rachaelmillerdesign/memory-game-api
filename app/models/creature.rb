4518d0db8563a1f36a541b7834849b8893e2f935
rachael miller design <rachael@rachaelmillerdesign.com>
2018-08-21 15:13:14 -0400

    added code to check database exists, it does!

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,19 +1,19 @@
 require 'csv'
 
 class Creature < ApplicationRecord
-  def initialize(h)
-    h.each_key do |key|
-      instance_variable_set(('@' + key.to_s).to_sym, h[key])
-      self.class.send(:attr_reader, key)
-    end
-  end
+  validates :common_name, uniqueness: :true
+  # def initialize(h)
+  #   h.each_key do |key|
+  #     instance_variable_set(('@' + key.to_s).to_sym, h[key])
+  #     self.class.send(:attr_reader, key)
+  #   end
+  # end
   def self.load_from_csv(csv_file)
     creatures = []
     CSV.foreach(csv_file,
             headers: true,
             header_converters: ->(h) { h.downcase.to_sym }) do |creature|
-              creatures << Creature.new(creature.to_hash)
+              creatures << Creature.create(creature.to_hash)
             end
-        end
-    end
+  end
 end
